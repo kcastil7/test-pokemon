e8925cf15a317f83ee62aa72b4b130672c4e530a
Kevin <kcas51@yahoo.com>
2022-02-01 12:57:37 -0800

    added pokemon api funtions

---
+++ script.js
@@ -0,0 +1,47 @@
+var searchEl = document.querySelector(".poke-name");
+var formEl = document.querySelector(".pokemon");
+var displayEl = document.querySelector(".pokemon-display");
+
+formEl.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var pokemonName = searchEl.value.trim();
+    console.log(pokemonName);
+
+    var requestUrl = 'https://pokeapi.co/api/v2/pokemon/' + pokemonName;
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayEl.textContent = "";
+            console.log(data);
+            console.log(data.name);
+            console.log(data.height);
+            console.log(data.weight);
+            console.log(data.sprites.front_default);
+
+            var name = document.createElement("h2");
+            name.textContent=data.name;
+            var img = document.createElement("img");
+            img.setAttribute("src",data.sprites.front_default);
+            var height = document.createElement("p");
+            height.textContent = "Height: " + data.height;
+            var weight = document.createElement("p");
+            weight.textContent = "Weight: " + data.weight;
+
+
+            displayEl.append(name);
+            displayEl.append(img);
+            displayEl.append(height);
+            displayEl.append(weight);
+
+
+        })
+        .catch(function (error) {
+            console.log(error);
+
+        })
+
+});
+
+
